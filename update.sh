7c28408e45c7dcecb2810afa36211b471ff4dca7
Ollin Boer Bohan <madebyollin@gmail.com>
2019-04-13 16:30:58 +0000

    catch errors during update and log updates

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 cd /root/arxiv-sanity-preserver
 python3 fetch_papers.py
 #python3 download_pdfs.py
@@ -9,3 +10,4 @@ python3 parse_pdf_to_text.py
 python3 analyze.py
 python3 buildsvm.py
 python3 make_cache.py
+echo "$(date) - $(whoami)" >> /root/arxiv-sanity-preserver/update_log.txt
