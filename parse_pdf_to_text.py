a92fa7744574e35d17b53c225753eb2a7fe37022
Ollin Boer Bohan <madebyollin@gmail.com>
2019-04-13 16:29:34 +0000

    Don't print as much during pdftotext

--- parse_pdf_to_text.py
+++ parse_pdf_to_text.py
@@ -28,7 +28,7 @@ files = os.listdir(Config.pdf_dir)
 for i,f in enumerate(files): # there was a ,start=1 here that I removed, can't remember why it would be there. shouldn't be, i think.
     txt_basename = f + '.txt'
     if txt_basename in have:
-        console.info('%d/%d skipping %s, already exists.' % (i, len(files), txt_basename, ))
+        # console.info('%d/%d skipping %s, already exists.' % (i, len(files), txt_basename, ))
         continue
 
     pdf_path = os.path.join(Config.pdf_dir, f)
